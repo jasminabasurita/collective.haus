b27110ee345ee1349445a2cbf87b64ef0e9b52b6
Jasmina Jacquelina <jasminejacquelin@gmail.com>
2018-03-22 18:09:56 -0400

    postgres-circleci

--- server/db/_db.js
+++ server/db/_db.js
@@ -1,6 +1,9 @@
 const Sequelize = require("sequelize")
-const db = new Sequelize("postgres://localhost:5432/collective-haus", {
-  logging: false
-})
+const db = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost:5432/collective-haus",
+  {
+    logging: false
+  }
+)
 
 module.exports = db
